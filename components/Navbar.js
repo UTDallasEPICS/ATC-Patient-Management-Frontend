68786a7dd07208e9843a460de1203f286fb0a943
Rigre Garciandia <rigrergl@outlook.com>
2021-02-14 23:11:50 -0600

    imported hamburger menu icon

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,11 @@
 import PropTypes from "prop-types";
 import styles from '../styles/Navbar.module.css'
+import { IoMenu } from "react-icons/io5"
 
 const Navbar = ({ pageTitle }) => {
     return (
         <div className={styles.banner}>
+            <IoMenu /> 
             <h1 className={styles.pageTitle}>{pageTitle}</h1>
         </div>
     )
