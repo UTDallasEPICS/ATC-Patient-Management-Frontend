b3e1b8cbd175a14a3f9ea45e75f9bd885e4c72e9
Rigre Garciandia <rigrergl@outlook.com>
2021-02-15 06:39:14 -0600

    added the image file input

--- pages/newStudent.js
+++ pages/newStudent.js
@@ -2,58 +2,63 @@ import Navbar from "../components/Navbar";
 import { useState } from "react";
 import styles from "../styles/NewStudent.module.css";
 import Link from "next/link";
-import Avatar from '../components/Avatar'
-
+import Avatar from "../components/Avatar";
+import { useForm } from "react-hook-form";
 
 export default function newStudent() {
   const [name, setName] = useState();
   const [age, setAge] = useState();
   const [otherInfo, setOtherInfo] = useState("");
 
-  //   let textarea = document.querySelector(".resizeTa");
-  //   textarea.addEventListener("keyup", () => {
-  //     textarea.style.height = calcHeight(textarea.value) + "px";
-  //   });
+  const { register, handleSubmit } = useForm();
 
   const resize = (e) => {
     e.target.style.height = calcHeight(e.target.value);
   };
+
+  const onSubmit = (data) => {
+    console.log(data);
+    //here is where the POST request will happen
+  };
+
   return (
     <div>
       <Navbar pageTitle="New Student" />
-      <div className={styles.inputs}>
-        <Avatar diameter="150px"/>
+      <div >
 
-        <input
-          className={styles.searchBox}
-          type="text"
-          placeholder="Name..."
-          onChange={(e) => {
-            setName(e.target.value);
-          }}
-        />
-        <input
-          className={styles.searchBox}
-          type="text"
-          placeholder="Age..."
-          onChange={(e) => {
-            setAge(e.target.value);
-          }}
-        />
-        <textarea
-          className={styles.textArea}
-          id="textArea"
-          type="text"
-          placeholder="Other Info..."
-          onChange={(e) => {
-            setOtherInfo(e.target.value);
-            resize(e);
-          }}
-        />
+        <form className={styles.inputs} onSubmit={handleSubmit(onSubmit)}>
 
-        <Link href="/">
+        <Avatar diameter="150px" />
+          <input ref={register} type="file" name="profilePicture"/>
+
+          <input
+            className={styles.searchBox}
+            type="text"
+            placeholder="Name..."
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+          <input
+            className={styles.searchBox}
+            type="text"
+            placeholder="Age..."
+            onChange={(e) => {
+              setAge(e.target.value);
+            }}
+          />
+          <textarea
+            className={styles.textArea}
+            id="textArea"
+            type="text"
+            placeholder="Other Info..."
+            onChange={(e) => {
+              setOtherInfo(e.target.value);
+              resize(e);
+            }}
+          />
           <button className={styles.button}>Done</button>
-        </Link>
+        </form>
       </div>
     </div>
   );
