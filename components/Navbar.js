0f08839be99ff4876177932e1c42898155ba1af6
Rigre Garciandia <rigrergl@outlook.com>
2021-02-15 04:26:10 -0600

    the side banner now spans the full vertical height

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 import styles from "../styles/Navbar.module.css";
 import { AiFillCaretDown, AiFillCaretUp } from "react-icons/ai";
 import Link from "next/link";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { TransitionGroup } from "react-transition-group";
 
 
@@ -43,6 +43,8 @@ const Navbar = ({ pageTitle }) => {
     }
   };
 
+  const windowSize = useWindowSize();
+
   return (
     <div className={styles.banner}>
       <div className={styles.menuToggle}>
@@ -51,11 +53,7 @@ const Navbar = ({ pageTitle }) => {
         <span></span>
         <span></span>
 
-        <ul className={styles.menu}>
-          {/* <Link href="/">
-            <li className={styles.link}>Students</li>
-          </Link> */}
-
+        <ul className={styles.menu} style={{height: windowSize.height}}>
           <button
             onClick={accordionClick}
             type="button"
@@ -94,6 +92,40 @@ const Navbar = ({ pageTitle }) => {
     </div>
   );
 };
+
+// Hook
+function useWindowSize() {
+    // Initialize state with undefined width/height so server and client renders match
+    // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+    const [windowSize, setWindowSize] = useState({
+      width: undefined,
+      height: undefined,
+    });
+  
+    useEffect(() => {
+      // only execute all the code below in client side
+      if (typeof window !== 'undefined') {
+        // Handler to call on window resize
+        function handleResize() {
+          // Set window width/height to state
+          setWindowSize({
+            width: window.innerWidth,
+            height: window.innerHeight,
+          });
+        }
+      
+        // Add event listener
+        window.addEventListener("resize", handleResize);
+       
+        // Call handler right away so state gets updated with initial window size
+        handleResize();
+      
+        // Remove event listener on cleanup
+        return () => window.removeEventListener("resize", handleResize);
+      }
+    }, []); // Empty array ensures that effect is only run on mount
+    return windowSize;
+  }
 
 Navbar.propTypes = {
   pageTitle: PropTypes.string.isRequired,
