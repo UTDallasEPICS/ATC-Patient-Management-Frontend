2c72c8c4941ffdf3cc1f0da984afba1a60d8f9a3
Rigre Garciandia <rigrergl@outlook.com>
2021-02-14 23:55:49 -0600

    weirdness demo

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,18 +1,29 @@
 import PropTypes from "prop-types";
-import styles from '../styles/Navbar.module.css'
-import { IoMenu } from "react-icons/io5"
+import styles from "../styles/Navbar.module.css";
+import { IoMenu } from "react-icons/io5";
+import Link from 'next/link'
 
 const Navbar = ({ pageTitle }) => {
-    return (
-        <div className={styles.banner}>
-            <IoMenu /> 
-            <h1 className={styles.pageTitle}>{pageTitle}</h1>
-        </div>
-    )
-}
+  return (
+    <div className={styles.banner}>
+      <div className={styles.menuToggle}>
+        <input type="checkbox" />
+        <span></span>
+        <span></span>
+        <span></span>
+
+        <ul className={styles.menu}>
+            <Link href="/"><li className={styles.link}>Students</li></Link>
+        </ul>
+      </div>
+
+      <h1 className={styles.pageTitle}>{pageTitle}</h1>
+    </div>
+  );
+};
 
 Navbar.propTypes = {
-    pageTitle: PropTypes.string.isRequired,
-  };
+  pageTitle: PropTypes.string.isRequired,
+};
 
-export default Navbar
+export default Navbar;
