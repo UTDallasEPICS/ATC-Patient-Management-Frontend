329c106da20a9dec9fc94ae89345ee70aade5fe5
Jeremiah Joseph <jeremiahjoseph@MacBook-Pro-3.local>
2021-02-15 23:50:02 -0600

    added login menu

---
+++ pages/Login.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import Image from 'next/image'
+import styles from '../styles/login.module.css'
+
+//Gets Login function and error function to help loggin in logic
+const Login = ({ loginf, error}) => {
+    const [details, setDetails] = useState({name: "", password: ""}); 
+    const submitHandler = e => {
+        e.preventDefault(); 
+
+        loginf(details); 
+    }
+    return (
+        <div>
+           <form onSubmit= {submitHandler}>
+                <img className = {styles.loginLogo} src = '/logo.jpeg' alt= 'logo'/> <br/>
+                <div className= {styles.loginData}>
+                    <label htmlfor= "name">Username:</label>
+                    <input type= "text" name= "name" id= "name" onChange={e => setDetails({...details, name: e.target.value})} value ={details.name}/> 
+                </div>
+                <div className= {styles.loginData}>
+                    <label htmlfor= "password">Password:</label>
+                    <input type= "text" name= "password" id= "password" onChange={e => setDetails({...details, password: e.target.value})} value ={details.password}/> 
+                </div> 
+
+                <input className = {styles.btn} type = "submit" value = "Sign In" />
+                <button className = {styles.btn}>Forgot Password</button> 
+            </form>
+            
+        </div>
+    )
+}
+
+export default Login
