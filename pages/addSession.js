61986f98c836d04dbefecc47e56531e78eaf68e9
Rigre Garciandia <rigrergl@outlook.com>
2021-02-25 18:47:51 -0600

    destructured program and mapped behvior titles

--- pages/addSession.js
+++ pages/addSession.js
@@ -4,10 +4,18 @@ import Container from "@material-ui/core/Container";
 import styles from "../styles/AddSession.module.css";
 
 const addSession = ({ program }) => {
+  const { studentID, firstName, lastName, behaviors } = program;
+
   return (
     <div>
       <Navbar pageTitle="Add Session" />
       <Container className={styles.container}>
+        <div>
+          {behaviors.map((behavior) => (
+            <h1>{behavior.title}</h1>
+          ))}
+        </div>
+
         <Button
           variant="contained"
           color="inherit"
@@ -25,10 +33,26 @@ export const getServerSideProps = async ({ query }) => {
 
   const program = {
     studentID: query.studentID,
-    behaviors: [{title: "LISTENER RESPONDING", description:" Will follow 20 1-step instructions, each for 3 trials a session across three consecutive sessions.", type: "probe", trialsPerEntry: 3, entries:["Shoulders", "Wiggle Fingers", "Pat Lap", "Touch Knees", "Stick Tongue"]}, 
-                {},
-                {title: "touch arm"},             
-              ],
+    firstName: "John",
+    lastName: "Doe",
+    behaviors: [
+      {
+        title: "LISTENER RESPONDING",
+        description:
+          " Will follow 20 1-step instructions, each for 3 trials a session across three consecutive sessions.",
+        type: "probe",
+        trialsPerEntry: 3,
+        entries: [
+          "Shoulders",
+          "Wiggle Fingers",
+          "Pat Lap",
+          "Touch Knees",
+          "Stick Tongue",
+        ],
+      },
+      { title: "Behavior 2" },
+      { title: "Behavior 3" },
+    ],
   };
 
   return {
