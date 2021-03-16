6192ec78e8f6652cf1caf08a2f0039725f4659d0
Jeremiah Joseph <jeremiahjoseph@MacBook-Pro-3.tx.rr.com>
2021-03-15 19:47:54 -0500

    updated student profile page

--- pages/studentProfile.js
+++ pages/studentProfile.js
@@ -7,9 +7,24 @@ import styles from "../styles/StudentProfile.module.css"
 import Divider from '@material-ui/core/Divider';
 import Button from "@material-ui/core/Button";
 import ButtonGroup from '@material-ui/core/ButtonGroup';
-
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Link from 'next/link';
 
 const studentProfile = ({ student }) => {
+    //State handles the notifications for when the archive is clicked
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
     
     return (
         <div className={styles.container}>
@@ -32,23 +47,40 @@ const studentProfile = ({ student }) => {
             <p className = {styles.label}>Other Info:</p>
             <OtherInfo info = {student.otherInfo}/>
             <div className ={styles.bg}>
+            <br/>
             <ButtonGroup variant="text" color="primary" aria-label="text primary button group">
-                <Button >Edit</Button>
-                <Button >New Session</Button>
-                <Button >View Reports</Button> 
-                <Button >Archive</Button>
+                <Button className= {styles.buttonGroup}>Edit</Button>
+                <Button className= {styles.buttonGroup}>New Session</Button>
+                <Button className= {styles.buttonGroup}>View Reports</Button> 
+                <Button className= {styles.buttonGroup} onClick={handleClickOpen}>Archive</Button>
             </ButtonGroup>
             </div>
+
+         <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+        >
+        <DialogTitle id="alert-dialog-title">{"Are you sure you want to archive this student?"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            When archiving a student you will no longer be able to view any of the records of the student. To regain access you will have to get access from an administration.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary" className={styles.buttonGroup}>
+            No
+          </Button>
+          <Button onClick={handleClose} color="primary" autoFocus className={styles.buttonGroup}>
+            <Link href="/studentSearch">
+            Yes
+            </Link>
+          </Button>
+        </DialogActions>
+      </Dialog>
            
-            <div className = {styles.buttonGrid}>
-                <button className={styles.btn}>Edit</button>
-                <button className={styles.btn} >Archive</button> <br/>
-            </div> 
-            <div className = {styles.buttonGrid2}>
-                <button className={styles.btn} >New Session</button> 
-                
-                <button className={styles.btn} >View Reports</button>
-                </div> <br/>
+            
             
             
             </div> <br/>
