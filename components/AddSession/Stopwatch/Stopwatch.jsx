5f1de631e4a3d9dc338a8f5b0715eeb1719cabc4
Rigre Garciandia <rigrergl@outlook.com>
2021-04-16 14:58:44 -0500

    reworked the way the timer works

--- components/AddSession/Stopwatch/Stopwatch.jsx
+++ components/AddSession/Stopwatch/Stopwatch.jsx
@@ -1,86 +1,50 @@
-import React from "react";
+import { useState } from "react";
 import ReactDOM from "react-dom";
 import StopwatchDisplay from "./StopwatchDisplay.jsx";
 import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
 import styles from "../../../styles/AddSession.module.css";
 
-class Stopwatch extends React.Component {
-  constructor(props) {
-    super(props);
+const Stopwatch = () => {
+  const [timerOn, setTimerOn] = useState(false);
+  const [timerTime, setTimerTime] = useState(0); 
 
-    this.state = {
-      running: false,
-      currentTimeMs: 0,
-      currentTimeSec: 0,
-      currentTimeMin: 0,
-    };
-  }
+  const  [timer, setTimer] = useState(null);
 
-  formatTime = (val, ...rest) => {
-    let value = val.toString();
-    if (value.length < 2) {
-      value = "0" + value;
-    }
-    if (rest[0] === "ms" && value.length < 3) {
-      value = "0" + value;
-    }
-    return value;
+  let centiseconds = ("0" + (Math.floor(timerTime / 10) % 100)).slice(-2);
+  let seconds = ("0" + (Math.floor(timerTime / 1000) % 60)).slice(-2);
+  let minutes = ("0" + (Math.floor(timerTime / 60000) % 60)).slice(-2);
+  let hours = ("0" + Math.floor(timerTime / 3600000)).slice(-2);
+
+
+  const start = () => {
+    setTimerOn(true);
+
+    setTimer(setInterval(() => {
+        setTimerTime((prevTime => prevTime + 10));
+    }, 10));
   };
 
-  start = () => {
-    if (!this.state.running) {
-      this.setState({ running: true });
-      this.watch = setInterval(() => this.pace(), 10);
-    }
+  const stop = () => {
+    setTimerOn(false);
+    clearInterval(timer);
+  };
+  const reset = () => {
+    setTimerTime(0);
   };
 
-  stop = () => {
-    this.setState({ running: false });
-    clearInterval(this.watch);
-  };
+  return (
+    <div>
+      <Card className={styles.durationInputBox}>
+        {timerOn === false && timerTime === 0 && <Button onClick={start}>START</Button>}
+        {timerOn === true && <Button onClick={stop}>STOP</Button>}
+        {timerOn === false &&  timerTime > 0 && <Button onClick={start}>RESUME</Button>}
+        <Button onClick={reset}>RESET</Button>
 
-  pace = () => {
-    console.log(this.props.i)
-    this.setState({ currentTimeMs: this.state.currentTimeMs + 10 });
-    if (this.state.currentTimeMs >= 1000) {
-      this.setState({ currentTimeSec: this.state.currentTimeSec + 1 });
-      this.setState({ currentTimeMs: 0 });
-    }
-    if (this.state.currentTimeSec >= 60) {
-      this.setState({ currentTimeMin: this.state.currentTimeMin + 1 });
-      this.setState({ currentTimeSec: 0 });
-    }
-  };
-
-  reset = () => {
-    this.setState({
-      currentTimeMs: 0,
-      currentTimeSec: 0,
-      currentTimeMin: 0,
-    });
-  };
-
-  render() {
-    return (
-      <div>
-        <Card className={styles.durationInputBox}>
-          {this.state.running === false && (
-            <Button onClick={this.start}>START</Button>
-          )}
-          {this.state.running === true && (
-            <Button onClick={this.stop}>STOP</Button>
-          )}
-          <Button onClick={this.reset}>RESET</Button>
-          <StopwatchDisplay
-            ref="display"
-            {...this.state}
-            formatTime={this.formatTime}
-          />
-        </Card>
-      </div>
-    );
-  }
-}
+        {hours}:{minutes}:{seconds}:{centiseconds}
+      </Card>
+    </div>
+  );
+};
 
 export default Stopwatch;
