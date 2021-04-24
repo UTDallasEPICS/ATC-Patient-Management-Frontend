46df6566e5e52afcff12c30bea08485095d744b7
Rigre Garciandia <rigrergl@outlook.com>
2021-04-23 20:08:56 -0500

    removed the multiple timer as for now we are assuming they only need one timer

--- components/AddSession/DurationInput.js
+++ components/AddSession/DurationInput.js
@@ -5,31 +5,12 @@ import styles from "../../styles/AddSession.module.css";
 import { useState, useEffect } from "react";
 
 const DurationInput = () => {
-  const [times, setTimes] = useState([]);
-  const [nextID, setNextID] = useState(0);
-
-  const addStopwatch = () => {
-    console.log("add stopwatch");
-    setTimes([...times, nextID]);
-    setNextID((prevID) => prevID + 1);
-  };
-
-  const stopwatches = times.map((id) => <Stopwatch id={id} key={id} />);
-
   return (
     <div>
-      {stopwatches}
-      <div style={{ display: "table", margin: "auto", marginTop: "20px" }}>
-        
-        <Fab
-          onClick={addStopwatch}
-          className={styles.addStopwatchButton}
-          size="medium"
-          color="primary"
-          aria-label="add"
-        >
-          <AddIcon />
-        </Fab>
+      <div
+        style={{ display: "table", margin: "auto", marginTop: "20px" }}
+      >
+        <Stopwatch/>
       </div>
     </div>
   );
